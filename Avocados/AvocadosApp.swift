f25e605aeaa654b23f4404ea51f2e017dcf6cd69
Labhesh <labhesh.dudi@gmail.com>
2025-10-26 10:40:07 +0530

    first commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Labhesh Dudi on 26/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Avocados/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Avocados
+//
+//  Created by Labhesh Dudi on 26/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
