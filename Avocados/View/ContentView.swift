618cdb9f58fab0c9095000016be6af47805564a1
Labhesh <labhesh.dudi@gmail.com>
2025-10-28 17:39:52 +0530

    headview layout

--- Avocados/View/ContentView.swift
+++ Avocados/View/ContentView.swift
@@ -9,10 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Avocado")
+        ScrollView(.vertical, showsIndicators: false) {
+            // MARK: - HEADER
+            
+            ScrollView(.vertical, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    HeaderView()
+                }
+            }
+            
+            // MARK: - FOOTER
+            
+            VStack(alignment: .center, spacing: 20) {
+                Text("All About Avocado")
+                    .font(.system(.title, design: .serif))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("ColorGreenAdaptive"))
+                    .padding(8)
+                Text("Everything you wanted to know about avocados but were too afraid to ask.")
+                    .font(.system(.body, design: .serif))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color.gray)
+            }
+            .frame(maxWidth: 640)
+            .padding()
+            .padding(.bottom, 85)
         }
-        .padding()
+        .edgesIgnoringSafeArea(.all)
+        .padding(0)
     }
 }
 
